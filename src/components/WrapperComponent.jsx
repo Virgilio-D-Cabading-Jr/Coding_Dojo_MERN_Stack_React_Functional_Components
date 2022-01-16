ce0ed21bdd350af94367140b57eb4c78ef8cda39
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-16 07:47:49 -0800

    add Wrapper Component

---
+++ src/components/WrapperComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+/**
+ * ///////////////////////////////////////////////////////
+ * WRAPPER COMPONENT
+ * ///////////////////////////////////////////////////////
+ * @returns wrapper
+ */
+const WrapperComp = () => {
+    return (
+        <div className='border'>
+            <p>Wrapper Component</p>
+        </div>
+    )
+}
+
+export default WrapperComp
