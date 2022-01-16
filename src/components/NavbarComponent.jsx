b7d1a2b6bed6e192f314adc6e72210ba00cefc38
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-16 10:51:30 -0800

    remove border around navbar component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -8,7 +8,7 @@ import React from 'react'
  */
 const NavbarComp = () => {
     return (
-        <div className='border p-2'>
+        <div>
             {/* //// HEADER //////// */}
             <header>
                 <div class="navbar navbar-dark bg-dark box-shadow">
