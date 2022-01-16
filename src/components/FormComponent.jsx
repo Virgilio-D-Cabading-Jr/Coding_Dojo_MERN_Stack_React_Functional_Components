57c1e08cc46cf4f838ecb37565066da2c88deedf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-16 06:56:50 -0800

    add FormComponent

---
+++ src/components/FormComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormComponent = () => {
+    return (
+        <div className='border'>
+            <p>Form Component</p>
+        </div>
+    )
+}
+
+export default FormComponent
